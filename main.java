b3a79da15653d05adece8e552461652438d1759a
tk2487 <39612327+tk2487@users.noreply.github.com>
2020-03-20 14:53:09 -0400

    Syntax correction
    Since main is void, created another function to return a boolean and main prints that value.

--- main.java
+++ main.java
@@ -1,23 +1,29 @@
 import java.util.HashMap;
 
 class Main {
-    public static boolean main(String[] args) {
-        String s1 = arg[0];
-        String s2 = arg[1];
-        if (s1.lenth != s2.length()) {
+    public static void main(String[] argv) {
+        String s1 = argv[0];
+        String s2 = argv[1];
+        System.out.println(mapping(s1, s2));
+    }
+
+    public static boolean mapping(String s1, String s2){
+        if (s1.length() != s2.length()) {
             return false;
         }
 
-        HashMap<Character, Character> map = new HashMap<Character, Character>(); // Map characters of string 1 to string 2
+        HashMap<Character, Character> map1 = new HashMap<Character, Character>(); // Map characters of string 1 to string 2
 
         for (int i = 0; i < s1.length(); i++) {
-            if (map.containsKey(s1.charAt(i))) {
-                if (map.get(s1.charAt(i)) != s2.charAt(i)) {
+            if (map1.containsKey(s1.charAt(i))) {
+                if (map1.get(s1.charAt(i)) != s2.charAt(i)) {
                     return false;
                 }
+            } else {
+                map1.put(s1.charAt(i), s2.charAt(i));
             }
         }
-
+        
         return true;
     }
 }
