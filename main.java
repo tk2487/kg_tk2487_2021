57285687fbb2f0c4aa6d9599ed166a50bd3e5914
tk2487 <39612327+tk2487@users.noreply.github.com>
2020-03-20 14:32:31 -0400

    Checks a duplicate key for correct value.
    If a key is already contained in the map, check to see if that key's value in the hashmap is the same as string 2's value at that specific position.

--- main.java
+++ main.java
@@ -1,1 +1,23 @@
+import java.util.HashMap;
 
+class Main {
+    public static boolean main(String[] args) {
+        String s1 = arg[0];
+        String s2 = arg[1];
+        if (s1.lenth != s2.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<Character, Character>(); // Map characters of string 1 to string 2
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (map.containsKey(s1.charAt(i))) {
+                if (map.get(s1.charAt(i)) != s2.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
